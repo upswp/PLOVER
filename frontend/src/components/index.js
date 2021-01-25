3552a85b39b2cb94de68684a78f6094c71533256
김나리 <rosenari88@gmail.com>
2021-01-26 00:30:34 +0900

    fix / S04P12B104-134 / 네비바,네비버튼 export

--- frontend/src/components/index.js
+++ frontend/src/components/index.js
@@ -1,2 +1,4 @@
 export { Skeleton } from "./Skeleton/Skeleton";
 export { Imgbox } from "./Imgcomponents/Imgbox";
+export { Navbar } from "./Navbar/Navbar";
+export { Navbutton } from "./Navbar/Navbutton";
