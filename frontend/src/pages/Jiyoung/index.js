f9fde535ab4e90998c6c3f19f648518eb777582f
Jiyoung <jayyyoung321@gmail.com>
2021-01-25 23:11:59 +0900

    feat / S04P12B104-149 / 자주 사용되는 아이콘 지정 및 개발

--- frontend/src/pages/Jiyoung/index.js
+++ frontend/src/pages/Jiyoung/index.js
@@ -4,6 +4,9 @@ function Page() {
     return (
         <div>
             지영페이지
+            <p>fontawesome_test</p>
+            <button><i class="fas fa-play-circle"></i></button>
+            <button><i class="fas fa-bars"></i></button>
         </div>
     )
 };
