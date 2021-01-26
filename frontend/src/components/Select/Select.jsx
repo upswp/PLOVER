75a95d1a9612f771eb1e51b9acb4da4a13ccb145
sycha <pepprbell@gmail.com>
2021-01-26 17:18:59 +0900

    feat / S04P12B104-146 / 셀렉트 박스 컴포넌트 개발

---
+++ frontend/src/components/Select/Select.jsx
@@ -0,0 +1,23 @@
+import React, { Component } from "react";
+import styles from "./Select.module.css";
+import classNames from "classnames/bind";
+
+const cx = classNames.bind(styles);
+
+class Select extends Component {
+    render() {
+        var short = false
+        if (this.props.short === "true") {
+            short = true
+        }
+        return (
+            <select className={cx('select',{'selectShort':short})}>
+                <option value="">SELECT</option>
+                <option value="a">a</option>
+                <option value="b">b</option>
+            </select>
+        )
+    }
+}
+
+export { Select };
