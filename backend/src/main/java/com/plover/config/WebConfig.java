65748a5359337b0d27198cf48a0675fd630c1a96
YunKihyeon <ykh094@naver.com>
2021-01-28 01:37:42 +0900

    refactor / S04P12B104-278 / CORS 설정

--- backend/src/main/java/com/plover/config/WebConfig.java
+++ backend/src/main/java/com/plover/config/WebConfig.java
@@ -11,7 +11,12 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowCredentials(true)
-                .allowedOrigins("*")
+                .allowedOrigins(
+                        "http://localhost:8080",
+                        "https://dev.plover.co.kr",
+                        "http://dev.plover.co.kr",
+                        "https://plover.co.kr",
+                        "http://plover.co.kr")
                 .allowedMethods("*")
                 .allowedHeaders("*");
     }
