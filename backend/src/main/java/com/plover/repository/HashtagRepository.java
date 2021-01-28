6a3b5c0d32334cb4f761c2acd9b27c1b2fe91e77
LEEAYOUNG <souk0711@gmail.com>
2021-01-28 10:36:50 +0900

    feat / S04P12B104-249 / 스터디관련 repository

--- backend/src/main/java/com/plover/repository/HashtagRepository.java
+++ backend/src/main/java/com/plover/repository/HashtagRepository.java
@@ -1,2 +1,11 @@
-package com.plover.repository;public class HashtagRepository {
+package com.plover.repository;
+
+import com.plover.model.study.Hashtag;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Set;
+
+public interface HashtagRepository extends JpaRepository<Hashtag,Long> {
+    List<Hashtag> findByNameIn(Set<String> names);
 }
--- backend/src/main/java/com/plover/repository/StudyRepository.java
+++ backend/src/main/java/com/plover/repository/StudyRepository.java
@@ -1,2 +1,15 @@
-package com.plover.repository;public class StudyRepository {
+package com.plover.repository;
+
+import com.plover.model.study.Study;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface StudyRepository extends JpaRepository<Study,Long> {
+    List<Study> findByNoticeIsTrueOrderByIdDesc(boolean isNotice, Pageable page);
+    List<Study> findByIdLessThanAndNoticeIsTrueOrderByIdDesc(Long id, boolean isNotice, Pageable page);
+    boolean existsByIdLessThan(Long id);
+    Optional<Study> findById(Long id);
 }
