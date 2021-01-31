28ff8b5788ef4da03faecbf73c2562ca77db59ba
임장순 <rosenari88@gmail.com>
2021-01-31 20:25:59 +0900

    feat / S04P12B104-259 / 페이지골격잡기(모바일)

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,12 @@
 import { Route } from 'react-router-dom';
 import { Jangsoon, Jiyoung, Suyeon } from './pages';
+import styles from "./App.module.css"
 import "./global.color.css";
 
 
 function App() {
   return (
-    <div>
+    <div className={styles.App}>
       <Route exact path="/" component={Jangsoon} />
       <Route path="/jangsoon" component={Jangsoon} />
       <Route path="/jiyoung" component={Jiyoung} />
