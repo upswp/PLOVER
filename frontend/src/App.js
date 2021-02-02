c6c404eeae8ed74a094aee5ff1a0fbcfc4e99de3
차수연 <pepprbell@gmail.com>
2021-02-02 12:40:57 +0900

    feat / S04P13B104-70 / 스터디 작성 페이지 생성

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,8 @@ import { Route } from 'react-router-dom';
 import {
   Jangsoon, Jiyoung, Suyeon,
   Register, Login,
-  MRegister, MList, MDetail
+  MRegister, MList, MDetail,
+  SRegister, SList, SDetail,
 } from './pages';
 import styles from "./App.module.css"
 import "./global.color.css";
@@ -20,6 +21,9 @@ function App() {
       <Route path="/mentoring/register" component={MRegister} />
       <Route path="/mentoring/list" component={MList} />
       <Route path="/mentoring/detail" component={MDetail} />
+      <Route path="/study/register" component={SRegister} />
+      <Route path="/study/list" component={SList} />
+      <Route path="/study/detail" component={SDetail} />
     </div>
   );
 }
--- frontend/src/pages/index.js
+++ frontend/src/pages/index.js
@@ -4,3 +4,4 @@ export { default as Suyeon } from "./Suyeon";
 export { default as Register } from "./Register";
 export { default as Login } from "./Login";
 export { MRegister, MList, MDetail } from "./Mentoring";
+export { SRegister, SList, SDetail } from "./Study";
