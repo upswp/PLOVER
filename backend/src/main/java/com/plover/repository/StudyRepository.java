0c242d83076fcbd74c1d0a23b69695c4dbf42c60
LEEAYOUNG <souk0711@gmail.com>
2021-02-05 23:03:42 +0900

    feat / S04P13B104-144 / study 검색기능 repository개발

--- backend/src/main/java/com/plover/repository/StudyRepository.java
+++ backend/src/main/java/com/plover/repository/StudyRepository.java
@@ -9,8 +9,14 @@ import java.util.Optional;
 import java.util.OptionalLong;
 
 public interface StudyRepository extends JpaRepository<Study,Long> {
+    // 리스트
     List<Study> findByIsNoticeOrderByIdDesc(boolean isNotice, Pageable page);
     List<Study> findByIdLessThanAndIsNoticeOrderByIdDesc(Long id, boolean isNotice, Pageable page);
-    boolean existsByIdLessThan(Long id);
+    // 검색
+    List<Study> findByIsNoticeAndTitleContainingOrderByIdDesc(boolean isNotice, String title, Pageable page);
+    List<Study> findByIdLessThanAndIsNoticeAndTitleContainingOrderByIdDesc(Long id, boolean isNotice, String title, Pageable page);
+
+    boolean existsByIsNoticeAndIdLessThan(boolean isNotice, Long id);
+    boolean existsByisNoticeAndIdLessThanAndTitleContaining(boolean isNotice, Long id, String title);
     Optional<Study> findById(Long id);
 }
