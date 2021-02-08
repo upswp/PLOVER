dced363e598467690b87d5dff780e69f4ddc548d
Jiyoung <jayyyoung321@gmail.com>
2021-02-08 21:05:03 +0900

    feat / S04P13B104-15 / 메뉴페이지개발

---
+++ frontend/src/components/Menu/MenuItems.jsx
@@ -0,0 +1,27 @@
+export const MenuItems = [
+  {
+    title: '홈',
+    url: '#',
+    cName: 'menu-links'
+  },
+  {
+    title: '친구 추천',
+    url: '#',
+    cName: 'menu-links'
+  },
+  {
+    title: '멘토링 클래스',
+    url: '#',
+    cName: 'menu-links'
+  },
+  {
+    title: '스터디 클래스',
+    url: '#',
+    cName: 'menu-links'
+  },
+  {
+    title: 'about US',
+    url: '#',
+    cName: 'menu-links'
+  }
+]
