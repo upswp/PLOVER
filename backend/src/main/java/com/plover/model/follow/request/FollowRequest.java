adc04a40b722718953aef21af6d9bf63cc5997e0
LEEAYOUNG <souk0711@gmail.com>
2021-02-09 13:35:31 +0900

    feat / S04P13B104-181 / follow관련 구조생성

--- backend/src/main/java/com/plover/model/follow/request/FollowRequest.java
+++ backend/src/main/java/com/plover/model/follow/request/FollowRequest.java
@@ -1,4 +1,25 @@
 package com.plover.model.follow.request;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+@Getter
+@ApiModel(description = "팔로우 모델")
 public class FollowRequest {
+    @ApiModelProperty(value = "팔로우를 누른 사람 이메일(현재로그인한 사용자)")
+    @Email(message = "이메일 형식이 아닙니다.")
+    @NotBlank(message = "반드시 입력해주세요.")
+    private String fromEmail;
+
+    @ApiModelProperty(value = "팔로잉 당하는 사람")
+    @Email(message = "이메일 형식이 아닙니다.")
+    @NotBlank(message = "반드시 입력해주세요.")
+    private String toEmail;
 }
--- backend/src/main/java/com/plover/repository/FollowRepository.java
+++ backend/src/main/java/com/plover/repository/FollowRepository.java
@@ -1,22 +1,8 @@
 package com.plover.repository;
 
-import com.plover.model.study.Study;
-import org.springframework.data.domain.Pageable;
+import com.plover.model.follow.Follow;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
-import java.util.Optional;
-import java.util.OptionalLong;
+public interface FollowRepository extends JpaRepository<Follow,Long> {
 
-public interface StudyRepository extends JpaRepository<Study,Long> {
-    // 리스트
-    List<Study> findByIsNoticeOrderByIdDesc(boolean isNotice, Pageable page);
-    List<Study> findByIdLessThanAndIsNoticeOrderByIdDesc(Long id, boolean isNotice, Pageable page);
-    // 검색
-    List<Study> findByIsNoticeAndTitleContainingOrderByIdDesc(boolean isNotice, String title, Pageable page);
-    List<Study> findByIdLessThanAndIsNoticeAndTitleContainingOrderByIdDesc(Long id, boolean isNotice, String title, Pageable page);
-
-    boolean existsByIsNoticeAndIdLessThan(boolean isNotice, Long id);
-    boolean existsByisNoticeAndIdLessThanAndTitleContaining(boolean isNotice, Long id, String title);
-    Optional<Study> findById(Long id);
 }
