7dadbd3f04ee6e93c32c07ddd16137f3964859a7
YunKihyeon <ykh094@naver.com>
2021-02-09 16:55:15 +0900

    fix / S04P12B104-193 / FCM 코드 수정

--- frontend/public/firebase-messaging-sw.js
+++ frontend/public/firebase-messaging-sw.js
@@ -2,12 +2,6 @@
 importScripts('https://www.gstatic.com/firebasejs/4.8.1/firebase-app.js');
 importScripts('https://www.gstatic.com/firebasejs/4.8.1/firebase-messaging.js');
 const config =  { 
-    apiKey: "AIzaSyC9k8y9PN9gCOQRM9lUx071p7-zCaRD6jo",
-    authDomain: "plover-e73ae.firebaseapp.com",
-    databaseURL: "https://plover-e73ae-default-rtdb.firebaseio.com",
-    projectId: "plover-e73ae",
-    storageBucket: "plover-e73ae.appspot.com",
-    messagingSenderId: "966814748963",
-    appId: "1:966814748963:web:ad6fdd53ce79fda72fbc96"
+    messagingSenderId: "966814748963"
 }; 
-firebase.initializeApp(config);
+const messaging = firebase.messaging();
