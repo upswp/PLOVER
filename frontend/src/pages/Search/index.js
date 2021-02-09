98c6f154e5a1644a8f4364e1962ed5c0ec665647
차수연 <pepprbell@gmail.com>
2021-02-09 17:11:09 +0900

    feat / S04P13B104-152 / 검색 페이지 구조 생성

--- frontend/src/pages/Search/index.js
+++ frontend/src/pages/Search/index.js
@@ -1,16 +1,67 @@
-import React from 'react';
-import {
+import React, { useState, useEffect } from 'react';
+import { Navbar, Tabs, Input
 //     // Skeleton, Cardslider, Badgeslider, Noticeslider, PulseBadge
-//     // , Badge, ButtonComp, Tabs, Input, ImgAttach, Select, ButtonComp, 
-//     // , InputTime, InputDate, Navbutton, Typo, Navbar,
+//     // , Badge, ButtonComp, Input, ImgAttach, Select, ButtonComp, 
+//     // , InputTime, InputDate, Navbutton, Typo
 } from "../../components";
 import styles from "./index.module.css";
+import axios from 'axios'
+import Event from "./event";
 
 function Search (props) {
 
+  const event = new Event(props.history)
+  useEffect(() => {
+    event.setKeyword(document.getElementById('propic'))
+    // event.setList(documen)
+  })
+
+
+  const [keyword, setKeyword] = useState('');
+  
+  const onKeywordHandler = (event) => {
+    setKeyword(document.getElementById('keyword'));
+    console.log(document.getElementById('keyword'))
+  };
+
+  const getList = () => {
+    console.log(this.state.value)
+    axios.get('https://dev.plover.co.kr/ssafy/study/search/0',{
+      params: {
+        keyword: this.state.value
+      }
+    })
+    .then ((res) => {
+      const posts = res
+      console.log(posts)
+      console.log(res.data)
+    })
+    .catch ((err) => {
+      console.log(err)
+    })
+  }
+
   return (
     <div className={styles.container}>
-      <p>search</p>
+      <div className={styles.head}>
+      <Navbar className={styles.nav}>
+        <i className={"fas fa-chevron-left color_black" + " " + styles.icon}></i>
+        <span className={"color_black" + " " + styles.title}><Input id="keyword" placeholder="검색" onChange={onKeywordHandler}></Input>
+        <button onClick={getList}>검색</button></span>
+        {/* <i className={"fas fa-search"}></i> */}
+      </Navbar>
+      <Tabs className={styles.tab}>
+        <div label="멘토링">
+        <p>TAB 1 contents</p>
+        </div>
+        <div label="스터디">
+        <p>TAB 2 contents</p>
+        </div>
+        <div label="사용자">
+        <p>TAB 3 contents</p>
+        </div>
+      </Tabs>
+      </div>
     </div>
   )
 }
