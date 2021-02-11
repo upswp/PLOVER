99c2ff44987b5df9771ecf9edc3f9371bcb2485e
YunKihyeon <ykh094@naver.com>
2021-02-11 22:39:44 +0900

    fix / S04P13B104-171 / fcm test

--- backend/src/main/java/com/plover/model/notification/Response/NotificationResponse.java
+++ backend/src/main/java/com/plover/model/notification/Response/NotificationResponse.java
@@ -1,5 +1,6 @@
 package com.plover.model.notification.Response;
 
+import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
@@ -8,5 +9,18 @@ import lombok.ToString;
 @Getter
 @ToString
 public class NotificationResponse {
+    private String token;
+    private String title;
+    private String message;
+    private String icon;
+    private String click_action;
 
+    @Builder
+    public NotificationResponse(String token, String title, String message, String icon, String click_action) {
+        this.token = token;
+        this.title = title;
+        this.message = message;
+        this.icon = icon;
+        this.click_action = click_action;
+    }
 }
