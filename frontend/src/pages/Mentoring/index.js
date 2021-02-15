71309a220fda8b2db90c7ede6f76e1ae04728891
임장순 <rosenari88@gmail.com>
2021-02-15 16:12:27 +0900

    feat / S04P13B104-236 / 멘토링수정페이지 생성

--- frontend/src/pages/Mentoring/index.js
+++ frontend/src/pages/Mentoring/index.js
@@ -1,3 +1,4 @@
 export { default as MDetail } from "./Detail";
 export { default as MList } from "./List";
 export { default as MRegister } from "./Register";
+export { default as MEdit } from "./Edit";
