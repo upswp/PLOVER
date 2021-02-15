e489c0b561e2495486acc853464f2228b732d8cc
차수연 <pepprbell@gmail.com>
2021-02-15 18:05:59 +0900

    feat / S04P13B104-163 / 이메일 확인 페이지 이벤트 연결

--- frontend/src/pages/Verify/index.js
+++ frontend/src/pages/Verify/index.js
@@ -1,9 +1,9 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useHistory } from 'react-router-dom';
 import {
-  Typo, Navbar,
+  Typo, Navbar, ButtonComp, 
 //     // Skeleton, Cardslider, Badgeslider, Noticeslider, PulseBadge
-//     // , Badge, ButtonComp, Tabs, Input, ImgAttach, Select, ButtonComp, 
+//     // , Badge, ButtonComp, Tabs, Input, ImgAttach, Select,
 //     // , InputTime, InputDate, Navbutton, 
 } from "../../components";
 import styles from "./index.module.css";
@@ -15,18 +15,22 @@ function Verify (props) {
 
   const history = useHistory()
 
-  const sendEmail = () => {
-    axios.post('https://dev.plover.co.kr/ssafy/account/verify',{
-      email: 'csy06@naver.com'
-    })
-    .then((res) => {
-      console.log(res)
-    }
-    )
-    .catch((err) => {
-      console.log(err)
-    })
-  }
+  // useEffect(() => {
+  //   sendEmail()
+  // }, [])
+
+  // function sendEmail() {
+  //   axios.post('https://dev.plover.co.kr/ssafy/account/verify',{
+  //     email: ''
+  //   })
+  //   .then((res) => {
+  //     console.log(res)
+  //   }
+  //   )
+  //   .catch((err) => {
+  //     console.log(err)
+  //   })
+  // }
 
   return (
     <div className={styles.container}>
@@ -36,9 +40,11 @@ function Verify (props) {
         <span className={"color_black" + " " + styles.title}><Typo ty="h4">회원가입 완료</Typo></span>
         <i></i>
       </Navbar>
-    <p>PLOVER의 회원이 되신 것을 환영합니다!</p>
-    <p>가입한 이메일 주소로 발송된 이메일을 확인해 주세요.</p>
-    <button onClick={sendEmail}>인증</button>
+      <div className={styles.notice}>
+        <p>PLOVER의 회원이 되신 것을 환영합니다!</p>
+        <p>가입한 이메일 주소로 발송된 이메일을 확인해 주세요.</p>
+      </div>
+    <ButtonComp type="base" width="regular" textvalue="재발송"></ButtonComp>
     </div>
   )
 }
