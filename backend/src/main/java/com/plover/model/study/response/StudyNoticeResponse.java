4789b2dc858cfea5e26fbc09bec8551a6cf2727a
LEEAYOUNG <souk0711@gmail.com>
2021-02-17 02:21:39 +0900

    fix / 스터디리스트 반환 데이터 추가

--- backend/src/main/java/com/plover/model/study/response/StudyNoticeResponse.java
+++ backend/src/main/java/com/plover/model/study/response/StudyNoticeResponse.java
@@ -4,6 +4,10 @@ import com.plover.model.study.Study;
 import lombok.Getter;
 import lombok.ToString;
 
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.time.temporal.ChronoUnit;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -14,13 +18,16 @@ public class StudyNoticeResponse {
 
     private String title;
 
-    public StudyNoticeResponse(Long id, String title) {
+    private String date;
+
+    public StudyNoticeResponse(Long id, String title, LocalDateTime createDate) {
         this.id = id;
         this.title = title;
+        this.date = countDay(createDate);
     }
 
     public static StudyNoticeResponse of(Study study) {
-        return new StudyNoticeResponse(study.getId(), study.getTitle());
+        return new StudyNoticeResponse(study.getId(), study.getTitle(),study.getCreateDate());
     }
 
     public static List<StudyNoticeResponse> listOf(List<Study> studies) {
@@ -30,5 +37,27 @@ public class StudyNoticeResponse {
             studyNoticeResponses.add(of(study));
         }
         return studyNoticeResponses;
+    }
+    public static String countDay(LocalDateTime createDate){
+        ZonedDateTime nowSeoul = ZonedDateTime.now(ZoneId.of("Asia/Seoul"));
+        LocalDateTime currDate = nowSeoul.toLocalDateTime();
+
+        StringBuilder period_s = new StringBuilder();
+        long period = 0;
+        if((period=createDate.until(currDate, ChronoUnit.YEARS))>0){
+            period_s.append(period).append("년전");
+        }else if((period=createDate.until(currDate, ChronoUnit.MONTHS))>0){
+            period_s.append(period).append("달전");
+        }else if((period=createDate.until(currDate, ChronoUnit.DAYS))>0){
+            period_s.append(period).append("일전");
+        }else if((period=createDate.until(currDate, ChronoUnit.HOURS))>0){
+            period_s.append(period).append("시간전");
+        }else if((period=createDate.until(currDate, ChronoUnit.MINUTES))>0){
+            period_s.append(period).append("분전");
+        }else{
+            period = createDate.until(currDate,ChronoUnit.SECONDS);
+            period_s.append(period).append("초전");
+        }
+        return period_s.toString();
     }
 }
