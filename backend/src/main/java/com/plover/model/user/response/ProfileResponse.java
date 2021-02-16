0e50a555fe10d5d0c17fe3724eec0f6559218c55
LEEAYOUNG <souk0711@gmail.com>
2021-02-16 10:12:40 +0900

    feat / S04P13B104-228 / 유저프로필dto개발

--- backend/src/main/java/com/plover/model/user/response/ProfileResponse.java
+++ backend/src/main/java/com/plover/model/user/response/ProfileResponse.java
@@ -1,4 +1,39 @@
 package com.plover.model.user.response;
 
+import com.plover.model.user.Users;
+import lombok.Getter;
+
+@Getter
 public class ProfileResponse {
+    private Long no;
+    private String email;
+    private String nickname;
+    private String generation;
+    private String description;
+    private Long articleNum; // 멘토링 게시글 수
+    private Long followerNum;
+    private Long followingNum;
+
+    public ProfileResponse(Long no, String email, String nickname, String generation, String description) {
+        this.no = no;
+        this.email = email;
+        this.nickname = nickname;
+        this.generation = generation;
+        this.description = description;
+    }
+
+    public static ProfileResponse of(Users user){
+        return new ProfileResponse(user.getNo(), user.getEmail(),user.getNickName(),user.getGeneration(),user.getDescription());
+    }
+
+    public void setArticleNum(Long articleNum){
+        this.articleNum = articleNum;
+    }
+
+    public void setFollowerNum(Long followerNum){
+        this.followerNum = followerNum;
+    }
+    public void setFollowingNum(Long followingNum){
+        this.followingNum = followingNum;
+    }
 }
