5e7dcc5415f8d21ee16ae6941ce4ff0e78243867
LEEAYOUNG <souk0711@gmail.com>
2021-02-16 13:59:46 +0900

    fix / S04P13B104-270 / 스터디 상세보기 반환 데이터에 프로필 사진 경로 추가

--- backend/src/main/java/com/plover/model/study/response/StudyDetailResponse.java
+++ backend/src/main/java/com/plover/model/study/response/StudyDetailResponse.java
@@ -11,13 +11,15 @@ import java.util.Set;
 @ToString
 public class StudyDetailResponse {
     private String nickName;
+    private String profileImageUrl;
     private String title;
     private String content;
     private Set<HashtagResponse> hashtags;
     private LocalDateTime createDate;
 
-    public StudyDetailResponse(String nickName, String title, String content, Set<HashtagResponse> hashtags, LocalDateTime createDate) {
+    public StudyDetailResponse(String nickName, String profileImageUrl,String title, String content, Set<HashtagResponse> hashtags, LocalDateTime createDate) {
         this.nickName = nickName;
+        this.profileImageUrl = profileImageUrl;
         this.title = title;
         this.content = content;
         this.hashtags = hashtags;
@@ -27,6 +29,7 @@ public class StudyDetailResponse {
     public static StudyDetailResponse of(Study study) {
         return new StudyDetailResponse(
                 study.getUser().getNickName(),
+                study.getUser().getProfileImageUrl(),
                 study.getTitle(),
                 study.getContent(),
                 HashtagResponse.listOf(study.getHashtags()),
