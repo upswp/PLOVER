28bdcc7bc34ac2cda5f4ec90097b012759fd1128
YunKihyeon <ykh094@naver.com>
2021-02-17 06:32:48 +0900

    feat / S04P13B104-294 / 알림 리스트(firebase)

--- backend/src/main/java/com/plover/service/NotificationService.java
+++ backend/src/main/java/com/plover/service/NotificationService.java
@@ -1,13 +1,25 @@
 package com.plover.service;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.plover.model.notification.FirebaseNotification;
 import com.plover.model.notification.Notification;
 import com.plover.model.notification.Response.NotificationResponse;
 import com.plover.model.user.Users;
 import com.plover.repository.NotificationRepository;
+import com.plover.utils.CookieUtil;
+import com.plover.utils.JwtUtil;
 import com.plover.utils.RedisUtil;
+import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.util.HashMap;
 import java.util.List;
 
 @Service
@@ -15,6 +27,10 @@ public class NotificationService {
     static final String FCM = "FCM_TOKEN_";
     @Autowired
     RedisUtil redisUtil;
+    @Autowired
+    CookieUtil cookieUtil;
+    @Autowired
+    JwtUtil jwtUtil;
     @Autowired
     NotificationRepository notificationRepository;
 
@@ -44,5 +60,18 @@ public class NotificationService {
 
     public void deleteNotification(final Long no){
         notificationRepository.deleteByNo(no);
+    }
+
+    public void postRealTimeDataBase(HttpServletRequest httpServletRequest, NotificationResponse notiResponse){
+        Cookie accessToken = cookieUtil.getCookie(httpServletRequest, JwtUtil.ACCESS_TOKEN_NAME);
+        String nickName = jwtUtil.getNickName(accessToken.getValue());
+        ZonedDateTime nowSeoul = ZonedDateTime.now(ZoneId.of("Asia/Seoul"));
+        LocalDateTime currDate = nowSeoul.toLocalDateTime();
+
+        final FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference ref = database.getReference().child("users").child(nickName);
+        DatabaseReference newPostRef = ref.push();
+        //TODO : 나중에 바꿔야함(경로)
+        newPostRef.setValueAsync(new FirebaseNotification(notiResponse.getMessage(),"https://dev.plover.co.kr/profile","0",currDate.toString()));
     }
 }
