905762c798de1b8c6effaea2e11451fe75fbcf10
Jiyoung <jayyyoung321@gmail.com>
2021-02-17 01:01:15 +0900

    feat / S04P13B104-282 / home 페이지study 부분 back 연결

--- frontend/src/components/StudyList/StudyListItem.jsx
+++ frontend/src/components/StudyList/StudyListItem.jsx
@@ -1,14 +1,22 @@
 import React from 'react';
+import Imgbox from '../Imgcomponents/Imgbox';
 import Skeleton from '../Skeleton/Skeleton';
 import styles from './StudyListItem.module.css';
 
 const StudyListItem = ({item}) => {
+  const renderProfileImg = () => {
+    if (item.profileImageUrl === 'images/default-image.png') {
+      return (<Skeleton shape="circle" className={styles.profile}/>)
+    } else {
+      return (<Imgbox src={item.profileImageUrl} size="tiny" shape="circle" className={styles.profile}/>)
+    }
+  }
 
   return (
     <div className={styles.study_list_item}>
-      <Skeleton shape="circle" className={styles.profile} />
+      {renderProfileImg()}
       <div className={styles.study_info}>
-        <h1 className={styles.study_username}>{item.user.userId}</h1>
+        <h1 className={styles.study_username}>{item.nickname}</h1>
         <p className={styles.study_time}>{item.date}</p>
       </div>
       <h2 className={styles.study_title}>{item.title}</h2>
