d15673b9b5a6a58ea3736c20e5970739fe035250
Park Sangwoo <tkddnsos@gmail.com>
2021-02-16 15:32:54 +0900

    fix / S04P13B104-252 / meet mentoring integer anotation 수정

--- backend/src/main/java/com/plover/model/metoring/meet/request/MeetMentoringInsertRequest.java
+++ backend/src/main/java/com/plover/model/metoring/meet/request/MeetMentoringInsertRequest.java
@@ -10,6 +10,7 @@ import org.hibernate.validator.constraints.Length;
 
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 
 @Getter
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
@@ -19,17 +20,17 @@ public class MeetMentoringInsertRequest extends InsertRequest {
     @NotBlank
     private String type;
 
-    @NotBlank
+    @NotNull
     private int currentPersonnel;
 
-    @NotBlank
+    @NotNull
     private int maxPersonnel;
 
     @NotBlank
     private String place;
 
     public MeetMentoringInsertRequest(@Email(message = "이메일 형식이 아닙니다.") @NotBlank(message = "반드시 입력해주세요") String email, @NotBlank @Length(max = 50, message = "제목의 최대 길이는 50자 입니다.") String title, @NotBlank @Length(max = 1000, message = "내용의 최대 길이는 1000자 입니다.") String content,
-                                      @NotBlank String startDate, @NotBlank String startTime, @NotBlank String endDate, @NotBlank String endTime, @NotBlank String type, @NotBlank int currentPersonnel, @NotBlank int maxPersonnel, @NotBlank String place) {
+                                      @NotBlank String startDate, @NotBlank String startTime, @NotBlank String endDate, @NotBlank String endTime, @NotBlank String type, @NotNull int currentPersonnel, @NotNull int maxPersonnel, @NotBlank String place) {
         super(email, title, content, startDate, startTime, endDate, endTime);
         this.type = type;
         this.currentPersonnel = currentPersonnel;
@@ -37,7 +38,7 @@ public class MeetMentoringInsertRequest extends InsertRequest {
         this.place = place;
     }
 
-    public MentoringEntity toMeet(){
+    public MentoringEntity toMeet() {
         return MentoringEntity.builder()
                 .type(type)
                 .currentPersonnel(currentPersonnel)
