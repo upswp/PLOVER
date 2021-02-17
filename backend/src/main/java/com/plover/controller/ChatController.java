7bb275799f354cb0acc9b523d7d6fb0e39a38194
LEEAYOUNG <souk0711@gmail.com>
2021-02-18 05:23:00 +0900

    fix / 메시지 전송 컬럼 수정

--- backend/src/main/java/com/plover/controller/ChatController.java
+++ backend/src/main/java/com/plover/controller/ChatController.java
@@ -1,12 +1,9 @@
 package com.plover.controller;
 
+import com.plover.model.Chat.request.ChatMessageRequest;
 import com.plover.model.Chat.request.ChatRequest;
 import com.plover.model.Response;
-import com.plover.model.user.Users;
-import com.plover.model.user.request.LoginRequest;
-import com.plover.model.user.response.LoginResponse;
 import com.plover.service.ChatService;
-import com.plover.utils.JwtUtil;
 import io.swagger.annotations.ApiOperation;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -18,10 +15,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import javax.validation.Valid;
 
 @RestController
 @RequestMapping("chat")
@@ -32,11 +26,22 @@ public class ChatController {
 
     @PostMapping("/send")
     @ApiOperation(value = "메세지 보내기",  notes = "1:1 메세지를 보냄", response = Response.class)
-    public Object sned(@RequestBody ChatRequest chatRequest,HttpServletRequest request) {
+    public Object send(@RequestBody ChatRequest chatRequest,HttpServletRequest request) {
 
 
         if(chatRequest!=null){
             chatService.postRealTimeDataBase(chatRequest, request);
+            return new ResponseEntity<>(new Response("success", "메세지 전송 성공", null), HttpStatus.OK);
+        }
+        else{
+            return new ResponseEntity<>(new Response("error", "메세지 전송 실패", null), HttpStatus.BAD_REQUEST);
+        }
+    }
+    @PostMapping("/message/send")
+    @ApiOperation(value = "메세지 보내기",  notes = "1:1 메세지를 보냄", response = Response.class)
+    public Object sendMessage(@RequestBody ChatMessageRequest chatMessageRequest, HttpServletRequest request) {
+        if(chatMessageRequest !=null){
+            chatService.postMessageRealTimeDataBase(chatMessageRequest, request);
             return new ResponseEntity<>(new Response("success", "메세지 전송 성공", null), HttpStatus.OK);
         }
         else{
--- backend/src/main/java/com/plover/service/ChatService.java
+++ backend/src/main/java/com/plover/service/ChatService.java
@@ -4,10 +4,10 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.plover.controller.AccountController;
 import com.plover.model.Chat.Chat;
+import com.plover.model.Chat.request.ChatMessageRequest;
 import com.plover.model.Chat.request.ChatRequest;
 import com.plover.utils.CookieUtil;
 import com.plover.utils.JwtUtil;
-import io.jsonwebtoken.Jwt;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -24,6 +24,8 @@ public class ChatService {
     JwtUtil jwtUtil;
     @Autowired
     CookieUtil cookieUtil;
+    @Autowired
+    UserService userService;
 
     private static final Logger logger = LoggerFactory.getLogger(AccountController.class);
 
@@ -46,6 +48,37 @@ public class ChatService {
         Chat chat = Chat.builder()
                 .massage(chatRequest.getMassage())
                 .profileImage(chatRequest.getProfileImage())
+                .writerNickName(writerNickName)
+                .sendTime(currDate.toString())
+                .build();
+
+        senderRef.setValueAsync(chat);
+        receiverRef.setValueAsync(chat);
+    }
+
+    public void postMessageRealTimeDataBase(ChatMessageRequest chatMessageRequest, HttpServletRequest request){
+        Long no = (long)jwtUtil.getNo(cookieUtil.getCookie(request, JwtUtil.ACCESS_TOKEN_NAME).getValue());
+        //내 no가지고오기
+        String fromUserNo = Long.toString(no);
+        //내 닉네임 가지고오기(writer에 들어갈거임)
+        String writerNickName = jwtUtil.getNickName(cookieUtil.getCookie(request, JwtUtil.ACCESS_TOKEN_NAME).getValue());
+        //내 사진 가지고오기
+        String writerProfileImage = userService.findUserByNo(no).getProfileImageUrl();
+
+        logger.info("보내는 사람번호는 : "+fromUserNo+" 보내는 사람 이름은 : "+writerNickName);
+
+        ZonedDateTime nowSeoul = ZonedDateTime.now(ZoneId.of("Asia/Seoul"));
+        LocalDateTime currDate = nowSeoul.toLocalDateTime();
+
+        final FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference ref = database.getReference().child("users").child("chat-plover-KKHH");
+
+        DatabaseReference senderRef = ref.child(fromUserNo).child(chatMessageRequest.getToUserNo()).push();
+        DatabaseReference receiverRef = ref.child(chatMessageRequest.getToUserNo()).child(fromUserNo).push();
+
+        Chat chat = Chat.builder()
+                .massage(chatMessageRequest.getMassage())
+                .profileImage(writerProfileImage)
                 .writerNickName(writerNickName)
                 .sendTime(currDate.toString())
                 .build();
