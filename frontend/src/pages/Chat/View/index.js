b6042f9088352304546cf8b2c9dc672ac8760402
임장순 <rosenari88@gmail.com>
2021-02-18 08:04:29 +0900

    fix / 채팅수정

--- frontend/src/pages/Chat/View/index.js
+++ frontend/src/pages/Chat/View/index.js
@@ -114,20 +114,18 @@ function View(props) {
                 <Input className={styles.chatting_input} placeholder="채팅메시지를 입력해주세요." id="chatinput" onkeypress={(e) => {
                     if (e.key === "Enter") {
                         restapi.post(`/chat/message/send`, {
-                            message: e.target.value,
+                            massage: e.target.value,
                             toUserNo: getIndex()
-                        }).then(res => {
-                            e.target.value = "";
-                        });
+                        })
+                        e.target.value = "";
                     }
                 }} />
                 <ButtonComp width="small" type="base" className={styles.chatting_btn} textvalue="전송" onClick={() => {
                     restapi.post(`/chat/message/send`, {
-                        message: document.getElementById("chatinput")[0].value,
+                        massage: document.getElementById("chatinput")[0].value,
                         toUserNo: getIndex()
-                    }).then(res => {
-                        document.getElementById("chatinput")[0].value = "";
                     });
+                    document.getElementById("chatinput")[0].value = "";
                 }} />
             </div>
         </div>
