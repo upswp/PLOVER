49888f5c1cb6563e65eb4e385ef2ab1bfbd47499
LEEAYOUNG <souk0711@gmail.com>
2021-02-17 21:13:24 +0900

    fix / 유저 로그인 반환 데이터 추가

--- backend/src/main/java/com/plover/model/user/response/LoginResponse.java
+++ backend/src/main/java/com/plover/model/user/response/LoginResponse.java
@@ -8,14 +8,16 @@ public class LoginResponse {
     private Long no;
     private String email;
     private String nickName;
+    private String profileImageUrl;
 
-    public LoginResponse(Long no, String email, String nickName) {
+    public LoginResponse(Long no, String email, String nickName,String profileImageUrl) {
         this.no = no;
         this.email = email;
         this.nickName = nickName;
+        this.profileImageUrl = profileImageUrl;
     }
 
     public static LoginResponse of(Users user){
-        return new LoginResponse(user.getNo(),user.getEmail(),user.getNickName());
+        return new LoginResponse(user.getNo(),user.getEmail(),user.getNickName(),user.getProfileImageUrl());
     }
 }
